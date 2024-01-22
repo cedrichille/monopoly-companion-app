cf90cc12428549be9e5ce273d60d3c19679ea5d8
Cedric Hille <cedrichille@gmail.com>
2024-01-21 18:03:39 -0800

    create first app files as flask tutorial

---
+++ app.py
@@ -0,0 +1,21 @@
+import re
+from datetime import datetime
+
+from flask import Flask
+from flask import render_template
+
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+    return "Hello, Flask!"
+
+@app.route("/hello/")
+@app.route("/hello/<name>")
+def hello_there(name = None):
+    return render_template(
+        "hello_there.html",
+        name=name,
+        date=datetime.now()
+    )
+
